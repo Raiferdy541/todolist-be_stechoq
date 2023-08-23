2875992ab6437afa71940d92f963f414777f601e
raiferdy541 <raihan.ferdyanza@gmail.com>
2023-08-23 11:57:21 +0700

    feat : create task,see task + verify jwt bearer token

--- app.js
+++ app.js
@@ -15,6 +15,23 @@ const db = mysql.createConnection({
   database: 'todo_list-jwt'
 });
 
+const verifyToken = (req, res, next) => {
+  const authHeader = req.header('Authorization');
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Tidak Terautentikasi' });
+  }
+
+  const token = authHeader.split(' ')[1]; // Ambil token setelah string 'Bearer'
+
+  try {
+    const decoded = jwt.verify(token, 'jwt'); 
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Token JWT Tidak Valid' });
+  }
+};
 
 app.post('/register', async (req, res) => {
   try {
@@ -47,6 +64,31 @@ app.post('/login', async (req, res) => {
     const token = jwt.sign({ userId: user[0].id }, 'jwt', { expiresIn: '1h' });
 
     res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Error' });
+  }
+});
+
+app.post('/todolist', verifyToken, async (req, res) => {
+  try {
+    const { userId } = req;
+    const { task, status } = req.body;
+
+    await db.promise().query('INSERT INTO todolist (user_id, task, status) VALUES (?, ?, ?)', [userId, task, status]);
+
+    res.json({ message: 'Task Berhasil Dibuat' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error' });
+  }
+});
+
+app.get('/todolist', verifyToken, async (req, res) => {
+  try {
+    const { userId } = req;
+
+    const [todolist] = await db.promise().query('SELECT * FROM todolist WHERE user_id = ?', [userId]);
+
+    res.json({ todolist });
   } catch (error) {
     res.status(500).json({ error: 'Error' });
   }
